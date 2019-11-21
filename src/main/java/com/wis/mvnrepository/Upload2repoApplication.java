65ec63472c7b80f83491e30a0063c8386467b64a
liubo <253620987@qq.com>
2019-11-21 10:27:50 +0800

    deploy

---
+++ src/main/java/com/wis/mvnrepository/Upload2repoApplication.java
@@ -0,0 +1,13 @@
+package com.wis.mvnrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Upload2repoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Upload2repoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wis/upload2repo/Upload2repoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wis.upload2repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Upload2repoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
